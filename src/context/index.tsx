11d8879a8bf5e3583c6e90f071851198bf867915
harrisonlars95 <harrisonlars95@users.noreply.github.com>
2024-09-21 12:20:05 +0800

    temp

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -28,8 +28,16 @@ export const modal = createAppKit({
   networks: [match],
   defaultNetwork: match,
   metadata: metadata,
-  includeWalletIds: ["38f5d18bd8522c244bdd70cb4a68e0e718865155811c043f052fb9f1c51de662"],
-  featuredWalletIds: ["38f5d18bd8522c244bdd70cb4a68e0e718865155811c043f052fb9f1c51de662"],
+  featuredWalletIds: [
+    "38f5d18bd8522c244bdd70cb4a68e0e718865155811c043f052fb9f1c51de662", // bgw
+    "c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96", // metamask
+    "4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0", // trustwallet
+  ],
+  includeWalletIds: [
+    "38f5d18bd8522c244bdd70cb4a68e0e718865155811c043f052fb9f1c51de662", // bgw
+    "c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96", // metamask
+    "4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0", // trustwallet
+  ],
   features: {
     socials: false,
     email: false,
