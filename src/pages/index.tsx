927baccb977e8d60c59c598aa1564145f02da062
harrisonlars95 <harrisonlars95@users.noreply.github.com>
2024-09-21 22:09:41 +0800

    ✨ Openlink2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,20 +26,33 @@ export default function Home() {
 
   return (
     <div className="p-1 flex flex-col text-wrap break-words">
-      <p>Tst: deeplink</p>
+      <p>Tst: openLink2</p>
       <button onClick={handleOpenModal}>Connect Bitget</button>
       <p>address: {address}</p>
 
-      <button onClick={() => signMessage({ message: "hello world" })}>Sign message</button>
+      <button
+        onClick={() => {
+          signMessage({ message: "hello world" });
+
+          (window as any).Telegram!.WebApp.openLink(`https://bkcode.vip`, {
+            try_browser: true,
+          });
+        }}
+      >
+        Sign message
+      </button>
       <p>result: {data}</p>
 
       <button
-        onClick={() =>
+        onClick={() => {
           sendTransaction({
             to: "0xd2135CfB216b74109775236E36d4b433F1DF507B",
             value: parseEther("0.01"),
-          })
-        }
+          });
+          (window as any).Telegram!.WebApp.openLink(`https://bkcode.vip`, {
+            try_browser: true,
+          });
+        }}
       >
         Send Transaction(transfer MAT)
       </button>
