df72e24f79f05c88e39ed9ab95125f21ac211134
harrisonlars95 <harrisonlars95@users.noreply.github.com>
2024-10-09 22:21:23 +0800

    temp

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -54,7 +54,7 @@ export default function Home() {
 
   return (
     <div className="p-1 flex flex-col text-wrap break-words">
-      <p>Tst: openLink2</p>
+      <p>Tst: 001</p>
       <button onClick={handleOpenModal}>Connect Bitget</button>
       <p>address: {address}</p>
 
